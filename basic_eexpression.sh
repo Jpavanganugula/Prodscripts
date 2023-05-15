dbfe67536a9c2bfeaec10787e586df01d982a69d
jeji <pavan935932@gmail.com>
2023-05-15 19:46:02 +0530

    basic1

---
+++ basic_eexpression.sh
@@ -0,0 +1,25 @@
+#expr 2 + 3
+#expr 2 \* 3    ###(* means wild card or wild symbols so use back slash \)
+#expr 22 / 2
+#
+#mail servicing from server to personal mail only
+#mailx -v admin.jeji@gmail.com
+#subjeect: node# prd1234 status
+#hi,
+#hello this mail regarsding status check for prod server
+#send me status
+.
+
+
+#service postfix status
+#service postfix reload
+#ifnot working chekc with network connectivity
+#
+#
+#if youbb want to send a  mail with attachment
+#mailx -a file1 -v admin.jeji@gmail.com
+#subject:
+#attached file with thi mail(maximum size of file is 10MB can attach in mail via server)
+#
+#regards,
+#jeji
